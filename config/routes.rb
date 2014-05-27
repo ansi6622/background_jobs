e07f8a8e9bff7588e4033a3dea23e06265f0dc6d
Mike Gehard <mike.gehard@gmail.com>
2014-05-27 10:54:13 -0600

    Make it easier to get to users/new

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Limit the routes that get created
   # This will not clutter rake routes with routes
   # we don't need.
+
+  root 'users#new'
   resources :users, only: [:new, :create]
 end
